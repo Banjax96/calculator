aa4873c94703b44daab4da0e636d25961a1d0128
Mark Milner <mmilner@activtech.com>
2024-10-21 17:18:59 -0400

    Add operate function

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,25 @@
+let num1;
+let operator;
+let num2;
 
+function operate(num1, num2, operand) {
+    let number1 = Number(num1);
+    let number2 = Number(num2);
+    switch(operand){
+        case "+":
+            return add(number1, number2);
+            break;
+        case "-":
+            return subtract(number1, number2);
+            break;
+        case "*":
+            return multiply(number1, number2);
+            break;
+        case "/":
+            return divide(number1, number2);
+            break;
+    }
+}
 
 function add(num1, num2){
     return num1 + num2;
