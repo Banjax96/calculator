5f25b1c5e4b1230c2a9e6bf5018c87f9692b9de5
Mark Milner <mmilner@activtech.com>
2024-11-01 15:38:11 -0400

    Add calculation functionality

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,35 @@ let num1;
 let operator;
 let num2;
 
+const operands = ['+', '-', '*', '/'];
+
+const h1 = document.querySelector("h1");
+const table = document.querySelector("#calculator");
+const btns = table.querySelectorAll("input");
+const screen = document.getElementById('screen');
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (btn.value != "=" && btn.value != "c") {
+            screen.value += btn.value;
+            if (operands.includes(btn.value)) {
+                operator = btn.value;
+            }
+        }
+        else if (btn.value == "=") {
+            let equation = screen.value;
+            let index = equation.indexOf(operator);
+            num1 = equation.substring(0, index);
+            num2 = equation.substring(index + 1)
+            screen.value = operate(num1, num2, operator);
+        }
+        else {
+            screen.value = "";
+        }
+        console.log(`Button value is: ${btn.value}`);
+    })
+});
+
 function operate(num1, num2, operand) {
     let number1 = Number(num1);
     let number2 = Number(num2);
